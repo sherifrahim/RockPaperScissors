4403b3a0df7cb7ed6fa74f08915fa5725c311f7f
Sherif Rahim <28657313+sherifrahim@users.noreply.github.com>
2022-05-02 18:10:26 +0530

    RCS:Add score conditions and Logics

--- rps.py
+++ rps.py
@@ -1,8 +1,15 @@
 from ast import Lambda
+from msilib import schema
 import random
 from tkinter import *
+from unittest import result
 
 #Var && Dictionaries
+schema = {
+    "rock":{"rock":1,"paper":0,"scissors":2},
+    "paper":{"rock":2,"paper":1,"scissors":0},
+    "scissors":{"rock":0,"paper":2,"scissors":1}
+}
 comp_score = 0
 player_score = 0
 
@@ -13,7 +20,25 @@ def outcome_handler(user_choice):
     outcomes = ["rock","paper","scissors"]
     random_number = random.randint(0,2)
     computer_choice = outcomes[random_number]
-    print(outcomes[random_number])
+    result = schema[user_choice][computer_choice]
+
+    player_choice_label.config(fg="red",text="Player Choice : "+str(user_choice))
+    computer_choice_label.config(fg="green",text="Computer Choice : "+str(computer_choice))
+
+    if result == 2:
+        player_score = player_score + 2
+        player_score_label.config(fg="blue",text="Player : "+str(player_score))
+        outcome_label.config(fg="blue",text="Outcome : Player Won")
+    elif result == 1:
+        player_score = player_score + 1
+        comp_score = computer_choice + 1
+        player_score_label.config(text="Player : "+str(player_score))
+        computer_score_label.config(text="Computer : "+str(comp_score))
+        outcome_label.config(fg="blue",text="Outcome : Draw")
+    elif result == 0:
+            comp_score = comp_score + 2
+            computer_score_label.config(text="Computer : "+str(comp_score))
+            outcome_label.config(fg="blue",text="Outcome : Computer Won")
 
 # Screen
 master = Tk()
